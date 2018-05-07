aab96666ae68946db78ec21c2e963cac52156256
RisaU <risa.ueda.0727@gmail.com>
2018-05-07 20:34:22 +1000

    学校での修正適応

--- www/js/function.js
+++ www/js/function.js
@@ -4,7 +4,7 @@ $(window).on('load',function(){
     var slide = $(".slideshow");
     var i = 0;
     // 1枚目のスライド表示
-    slide.find(".item").eq(i).fadeIn(2000).addClass("in");
+    slide.find(".item").eq(i).fadeIn(1000).addClass("in");
     // スライドの枚数
     var total = $(".slideshow .item").length -1;
 
@@ -16,22 +16,22 @@ $(window).on('load',function(){
                 j = i;
                 i++;
                 setTimeout(function() {
-                    slide.find(".item").eq(i).fadeIn(5500)
+                    slide.find(".item").eq(i).fadeIn(6500)
                     .addClass("in").removeClass("out");
                 }, 1000);
-            slide.find(".item").eq(j).fadeOut(6500);
+            slide.find(".item").eq(j).fadeOut(7500);
         } else if(i == total) {
             slide.find(".item").eq(i).addClass("out");
             slide.find(".item").eq(i).removeClass("in");
             j = i;
             i = 0;
             setTimeout(function() {
-                slide.find(".item").eq(i).fadeIn(5500)
+                slide.find(".item").eq(i).fadeIn(6500)
                     .addClass("in").removeClass("out");
             }, 1000);
-            slide.find(".item").eq(j).fadeOut(6500);
+            slide.find(".item").eq(j).fadeOut(7500);
         };
-    }, 6500);
+    }, 7500);
 });
 /*  
 http://imasashi.net/element-fadein.html
