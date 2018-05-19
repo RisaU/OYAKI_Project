455dbd963aa2947ed527642d418ca7451c23e686
RisaU <risa.ueda.0727@gmail.com>
2018-05-19 22:19:31 +1000

    Add slideshow for menu gallery

--- www/js/function.js
+++ www/js/function.js
@@ -1,10 +1,15 @@
 $(window).on('load',function(){
 // load: 画像などのデータ全ての読み込みが完了したら実行
 
+/**
+* http://yurixxx8.hatenablog.com/entry/2017/02/05/022049
+* プラグインなしで拡大しながらフェードで切り替わるスライド
+*/
+
     var slide = $(".slideshow");
     var i = 0;
     // 1枚目のスライド表示
-    slide.find(".item").eq(i).fadeIn(1000).addClass("in");
+    slide.find(".item").eq(i).fadeIn(2000).addClass("in");
     // スライドの枚数
     var total = $(".slideshow .item").length -1;
 
@@ -16,22 +21,25 @@ $(window).on('load',function(){
                 j = i;
                 i++;
                 setTimeout(function() {
-                    slide.find(".item").eq(i).fadeIn(6500)
+					// 1000ミリ秒後に3500ミリ秒かけてフェードインする
+                    slide.find(".item").eq(i).fadeIn(3500)
                     .addClass("in").removeClass("out");
                 }, 1000);
-            slide.find(".item").eq(j).fadeOut(7500);
+			// 4500ミリ秒かけてフェードアウトする
+            slide.find(".item").eq(j).fadeOut(4500);
         } else if(i == total) {
             slide.find(".item").eq(i).addClass("out");
             slide.find(".item").eq(i).removeClass("in");
             j = i;
             i = 0;
             setTimeout(function() {
-                slide.find(".item").eq(i).fadeIn(6500)
+                slide.find(".item").eq(i).fadeIn(3500)
                     .addClass("in").removeClass("out");
             }, 1000);
-            slide.find(".item").eq(j).fadeOut(7500);
+            slide.find(".item").eq(j).fadeOut(4500);
         };
-    }, 7500);
+	// この値を変更すると、処理の間隔を遅くしたり早くしたりできる
+    }, 7000);
 });
 /*  
 http://imasashi.net/element-fadein.html
